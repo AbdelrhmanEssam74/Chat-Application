27f412be1f4c2e23696fda4bad6379f172a02caf
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-07-05 19:39:45 +0300

    "Added username setter form to index.php"

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
     <div class="row">
       <div class="col-lg-6  col-mid-12">
         <h2>Chat Application</h2>
+        <div class="row">
+          <form action="" class="username-setter" method="post">
+              <div class="form-group">
+                <label for="">Set Username</label>
+                <input type="text" name="name" value="" class="form-control username-input">
+              </div>
+              <button class="btn btn-primary " type="submit">Set</button>
+          </form>
+        </div>
         <h3>Messages</h3>
         <ul class="message_list">
         </ul>
