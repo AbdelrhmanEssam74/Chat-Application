dc94953ab26e80338d203fc9cc35d94c20e58497
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-07-16 20:38:06 +0300

    remove navbar component from header.php file and add navBar.php file in profile file

--- include/template/header.php
+++ include/template/header.php
@@ -40,27 +40,3 @@ if ($page == 'login' || $page == 'register') {
 </head>
 
 <body>
-  <?php
-  if ($page != 'login' && $page != 'register') {
-  ?>
-    <nav>
-      <input type="checkbox" id="check">
-      <label for="check" class="checkbtn">
-        <i class="fas fa-bars"></i>
-      </label>
-      <label class="logo">Chat App</label>
-      <ul>
-        <li><a  href="<?php echo $ChatRoom ?>">Home</a></li>
-        <!-- <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Contact</a></li>-->
-        <li><a id="profile" href="Profile.php">Profile</a></li>
-        <li><a id="logout" href="<?php echo $authentication ?>logout.php">Logout</a></li>
-      </ul>
-    </nav>
-  <?php
-  }
-  ?>
-  <div id="loadingIcon" style="display: none;">
-    <div class="loader"></div>
-  </div>
