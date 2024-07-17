fbe219ff2bd23ff76a5f1d0c8a8b0cf15add44d7
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-07-17 19:23:37 +0300

    commit Update chat functionality
    This commit introduces several updates to the chat functionality:
    
    *   It clarifies that one needs to implement the connection interface to be able to interact with individual client connections in the Ratchet server.
    *   It updates the onMessage method to include the time at which a message was sent and the receiver of the message when creating a message in the DB.
    *   It provides the option of sending a response with a date when a message is received.
    *   It adds support for receiver and time fields in the message model.

--- src/Chat.php
+++ src/Chat.php
@@ -6,76 +6,84 @@ require 'vendor/autoload.php';
 //NOTE - This is an interface provided by the Ratchet library.
 //NOTE -  It defines the methods that a class must implement to act as a message component 
 use Ratchet\MessageComponentInterface;
-//NOTE - It defines the methods that a class must implement to represent a connection in a Ratchet server. The connection interface allows you to interact with individual client connections, such as sending data or closing the connection 
+
+//NOTE - It defines the methods that a class must implement to represent a connection in a Ratchet server. The connection interface allows you to interact with individual client connections, such as sending data or closing the connection
 use Ratchet\ConnectionInterface;
 use ChatApp\models\message;
 
 class Chat implements MessageComponentInterface
 {
-  protected $clients;
-  public function __construct()
-  {
-    $this->clients = new \SplObjectStorage;
-  }
-  /**
-   * Called when a new connection is opened.
-   *
-   * @param ConnectionInterface $connection The newly opened connection
-   */
-  public function onOpen(ConnectionInterface $connection)
-  {
-    $this->clients->attach($connection);
-    echo "New Connection! " . $connection->resourceId . "\n";
-  }
+    protected $clients;
 
-  /**
-   * Called when a message is received from a connection.
-   *
-   * @param ConnectionInterface $from    The connection from which the message was received
-   * @param string              $msg The received message
-   */
-  public function onMessage(ConnectionInterface $from, $msg)
-  {
-    $msg = json_decode($msg);
-    switch ($msg->type) {
-      case 'message':
-        foreach ($this->clients as $client) : // for each through all the clients that are currently connected in the server and send a message 
-          if ($client !== $from) :
-            $client->send($msg->text);
-          endif;
-        endforeach;
-        // store the message in the DB
-        message::create([
-          "text" => $msg->text,
-          "sender" => $msg->sender
-        ]);
-        break;
-      default:
-        # code...
-        break;
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
     }
-  }
 
-  /**
-   * Called when a connection is closed.
-   *
-   * @param ConnectionInterface $conn The closed connection
-   */
-  public function onClose(ConnectionInterface $conn)
-  {
-    $this->clients->detach($conn);
-    echo "Connection {{$conn->resourceId}} has disconnected\n";
-  }
+    /**
+     * Called when a new connection is opened.
+     *
+     * @param ConnectionInterface $connection The newly opened connection
+     */
+    public function onOpen(ConnectionInterface $connection)
+    {
+        $this->clients->attach($connection);
+        echo "New Connection! " . $connection->resourceId . "\n";
+    }
 
-  /**
-   * Called when an error occurs on a connection.
-   *
-   * @param ConnectionInterface $connection The connection on which the error occurred
-   * @param \Exception          $e          The error exception
-   */
-  public function onError(ConnectionInterface $connection, \Exception $e)
-  {
-    echo "Error : {$e->getMessage()}";
-    $connection->close();
-  }
+    /**
+     * Called when a message is received from a connection.
+     *
+     * @param ConnectionInterface $from The connection from which the message was received
+     * @param string $msg The received message
+     */
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $msg = json_decode($msg);
+        switch ($msg->type) {
+            case 'message':
+                foreach ($this->clients as $client) : // for each through all the clients that are currently connected in the server and send a message
+                    if ($client !== $from) :
+                        $response = [
+                            'msg' => ($msg->msg),
+                            "date" =>($msg->date),
+                        ];
+                    $client->send(json_encode($response));
+                    endif;
+                endforeach;
+                // store the message in the DB
+                message::create([
+                    "text" => $msg->msg,
+                    "time"=>$msg->date,
+                    "sender" => $msg->user_id,
+                    "receiver" => $msg->receiver]);
+                break;
+            default:
+                # code...
+                break;
+        }
+    }
+
+    /**
+     * Called when a connection is closed.
+     *
+     * @param ConnectionInterface $conn The closed connection
+     */
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+        echo "Connection {{$conn->resourceId}} has disconnected\n";
+    }
+
+    /**
+     * Called when an error occurs on a connection.
+     *
+     * @param ConnectionInterface $connection The connection on which the error occurred
+     * @param \Exception $e The error exception
+     */
+    public function onError(ConnectionInterface $connection, \Exception $e)
+    {
+        echo "Error : {$e->getMessage()}";
+        $connection->close();
+    }
 }
--- src/models/message.php
+++ src/models/message.php
@@ -2,5 +2,5 @@
 namespace ChatApp\models;
 final class message extends \Illuminate\Database\Eloquent\Model
 {
-  protected $fillable = ['text' , 'sender'];
+  protected $fillable = ['text' , 'sender' , 'receiver' , 'time'];
 }
